704b4f466e9ed2662ac6bc05648f64b2015824ae
terse <okekechidera992@gmail.com>
2025-07-12 10:49:40 +0100

    mergd two branchs twog

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/notfound.css"; // Assuming you have a CSS file for styles
+import "../styles/notFound.css"; // Assuming you have a CSS file for styles
 
 const NotFound = () => {
   return (
